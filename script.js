7debfd1474222539facb6a33cb25b39f7fe02ee2
Ipek Turgul <ipek.turgul@gmail.com>
2015-09-09 20:29:01 +1000

    add autoslider function

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-(function() {
+(function () {
 
-/*
-TO DO
-one appear others hidden
-set timeout 500 it changes
-change is smooth
-change can be controlled by arrows
-change can be controlled by dots.
-dots indicate which image is seen
-touch control possible
+  var auto = true,
+    images = Array.prototype.slice.call($('.img_list li'));
 
-*/
-
-  var images = Array.prototype.slice.call($('.img_list li'));
-
-  var slide = function(target) {
-     var currentElement = $('.active');
+  var slide = function (target) {
+    var currentElement = $('.active');
     currentElement.removeClass('active fadeIn')
-        .addClass('fadeOut');
+      .addClass('fadeOut');
     target.addClass('active fadeIn');
 
     currentElement.removeClass('hide fadeOut');
@@ -34,45 +23,63 @@ touch control possible
   };
 
   var nextElement = function (arg1, arg2) {
-     var nextElement = $(arg1).next();
-      if (nextElement.length === 0) {
-        nextElement = $(arg2).first();
-      }
+    var nextElement = $(arg1).next();
+    if (nextElement.length === 0) {
+      nextElement = $(arg2).first();
+    }
     return nextElement;
   };
 
   var prevElement = function (arg1, arg2) {
     var prevElement = $(arg1).prev();
-      if (prevElement.length === 0) {
-        prevElement = $(arg2).last();
-      }
-      console.log(prevElement);
+    if (prevElement.length === 0) {
+      prevElement = $(arg2).last();
+    }
+    console.log(prevElement);
     return prevElement;
   };
+
+  var autoSlide = function () {
+    slide(nextElement('li.active', '.img_list  li'));
+    slider_control(nextElement('li.current', '.slider_control li'));
+  };
+
+  function timeout() {
+    setTimeout(function () {
+      if (auto) {
+        autoSlide();
+        timeout();
+      } else {
+        return;
+      }
+    }, 3000);
+  }
 
 
   $(function () {
 
-    $('.back').on('click', function() {
+    $('.back').on('click', function () {
 
       slide(prevElement('li.active', '.img_list li'));
       slider_control(prevElement('li.current', '.slider_control li'));
 
     });
 
-    $('.forward').on('click', function() {
-
+    $('.forward').on('click', function () {
+      auto = false;
       slide(nextElement('li.active', '.img_list  li'));
       slider_control(nextElement('li.current', '.slider_control li'));
 
     });
 
-    $('.slider_control_item').on('click', function() {
+    $('.slider_control_item').on('click', function () {
+      auto = false;
       var target = $(this).attr('data-imgref');
       slide($(target));
     });
 
+    timeout();
+
   });
 
 })();
-
